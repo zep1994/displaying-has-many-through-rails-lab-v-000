a9bbd402c5be695342a8ab2bbe683c650415ec43
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-10-02 16:42:29 +0000

    automatically backed up by learn

--- db/migrate/20181002161234_create_appointments.rb
+++ db/migrate/20181002161234_create_appointments.rb
@@ -2,8 +2,8 @@ class CreateAppointments < ActiveRecord::Migration
   def change
     create_table :appointments do |t|
       t.datetime :appointment_datetime
-      t.belongs_to :doctor_id
-      t.belongs_to :patient, index: true, foreign_key: true 
+      t.integer :doctor_id
+      t.integer :patient_id
     end
   end
 end
