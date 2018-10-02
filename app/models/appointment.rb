d7cf3471896159093e86f94a0031f872c77e3068
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-10-02 16:45:43 +0000

    automatically backed up by learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,10 +1,4 @@
-require 'date'
-require 'time'
-
 class Appointment < ActiveRecord::Base
-  belongs_to :doctor
-  belongs_to :patient
-
-  def appointment_datetime_to_s
-    self.appointment_datetime.strftime("%B %e,%Y at %k:%M")
-  end
+    belongs_to :doctor
+    belongs_to :patient
+end
